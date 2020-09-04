22966ecf705aae40183b3e5918ccba3605c73fdb
yu <watanabeyuu@MacBook-Pro.local>
2020-09-05 00:00:24 +0900

    test_auth

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,52 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", :type => :request do
-
-  describe "/api/v1/posts" do
-   
-   it "10取得できるかの確認 index" do
+RSpec.describe 'Posts', type: :request do
+  describe '/api/v1/posts' do
+    it '10取得できるかの確認 index' do
       FactoryBot.create_list(:post, 10)
-      get "/api/v1/posts"
+      get '/api/v1/posts'
       expect(response.status).to eq(200)
       json = JSON.parse(response.body)
       expect(json.length).to eq(10)
-     
-      #p response.status
-      #p json
-   end
 
-   it "特定のpostを取得できるかの確認 show" do
-     post = FactoryBot.create(:post, :title => "test-title")
+      # p response.status
+      # p json
+    end
 
-     get "/api/v1/posts/#{post.id}"
-     json = JSON.parse(response.body)
-     expect(response.status).to eq(200)
-     expect(json["title"]).to eq(post.title)
-     
-     #p response.status
-     #p json
-   end
+    it '特定のpostを取得できるかの確認 show' do
+      post = FactoryBot.create(:post, title: 'test-title')
 
-   it "postが作成できるかの確認 create" do
-     post_params = { :title => "test_spec" }
-     expect { post "/api/v1/posts", :params => { :post => post_params } }.to change(Post, :count).by(+1)
-     expect(response.status).to eq(200)
+      get "/api/v1/posts/#{post.id}"
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(json['title']).to eq(post.title)
 
-     #p response.headers
-   end
+      # p response.status
+      # p json
+    end
 
-   it "postの編集を行う　put" do
-     post = FactoryBot.create(:post, :title => "old_title")
-     put "/api/v1/posts/#{post.id}", :params => {:post => {:title => "new_title"} }
-     json = JSON.parse(response.body)
-     expect(response.status).to eq(200)
-     expect(json["title"]).to eq("new_title")
-   end
+    it 'postが作成できるかの確認 create' do
+      user = { email: 'test@test.com', password: '123456' }
+      auth_tokens = sign_in(user)
 
-   it "postを削除できるかの確認 delete" do
-     post = FactoryBot.create(:post, :title => "delete_test")
-     delete "/api/v1/posts/#{post.id}"
-     expect(response.status).to eq(200)
-   end
+      post_params = { title: 'test_spec' }
+      expect { post '/api/v1/posts', params: { post: post_params }, headers: { headers: auth_tokens } }.to change(Post, :count).by(+1)
+      expect(response.status).to eq(200)
+
+      # p response.headers
+    end
+
+    it 'postの編集を行う　put' do
+      post = FactoryBot.create(:post, title: 'old_title')
+      put "/api/v1/posts/#{post.id}", params: { post: { title: 'new_title' } }
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(json['title']).to eq('new_title')
+    end
+
+    it 'postを削除できるかの確認 delete' do
+      post = FactoryBot.create(:post, title: 'delete_test')
+      delete "/api/v1/posts/#{post.id}"
+      expect(response.status).to eq(200)
+    end
   end
 end
