b32e3be7e1529d719ca4cd309dcc553ecce0e72b
cat3582 <cstbwy@gmail.com>
2020-10-01 00:09:19 +0900

    user_ref

--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_144504) do
+ActiveRecord::Schema.define(:version => 2020_09_30_144539) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table "active_storage_attachments", :options => "ENGINE=InnoDB DEFAULT CHARSET=utf8", :force => :cascade do |t|
+    t.string "name", :null => false
+    t.string "record_type", :null => false
+    t.bigint "record_id", :null => false
+    t.bigint "blob_id", :null => false
+    t.datetime "created_at", :null => false
+    t.index ["blob_id"], :name => "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], :name => "index_active_storage_attachments_uniqueness", :unique => true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
+  create_table "active_storage_blobs", :options => "ENGINE=InnoDB DEFAULT CHARSET=utf8", :force => :cascade do |t|
+    t.string "key", :null => false
+    t.string "filename", :null => false
     t.string "content_type"
     t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    t.bigint "byte_size", :null => false
+    t.string "checksum", :null => false
+    t.datetime "created_at", :null => false
+    t.index ["key"], :name => "index_active_storage_blobs_on_key", :unique => true
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "images", :options => "ENGINE=InnoDB DEFAULT CHARSET=utf8", :force => :cascade do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "posts", :options => "ENGINE=InnoDB DEFAULT CHARSET=utf8", :force => :cascade do |t|
     t.string "title"
     t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.bigint "user_id", :null => false
+    t.index ["user_id"], :name => "index_posts_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+  create_table "users", :options => "ENGINE=InnoDB DEFAULT CHARSET=utf8", :force => :cascade do |t|
+    t.string "provider", :default => "email", :null => false
+    t.string "uid", :default => "", :null => false
+    t.string "encrypted_password", :default => "", :null => false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
+    t.boolean "allow_password_change", :default => false
     t.datetime "remember_created_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
@@ -62,18 +64,18 @@ ActiveRecord::Schema.define(version: 2020_08_27_144504) do
     t.string "image"
     t.string "email"
     t.text "tokens"
-    t.integer "sign_in_count", default: 0, null: false
+    t.integer "sign_in_count", :default => 0, :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.index ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+    t.index ["email"], :name => "index_users_on_email", :unique => true
+    t.index ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+    t.index ["uid", "provider"], :name => "index_users_on_uid_and_provider", :unique => true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", :column => "blob_id"
 end
