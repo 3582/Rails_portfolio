b87c32c1a5523a8e3aadb5228f4c4afaea5d4717
cat3582 <cstbwy@gmail.com>
2020-11-17 21:22:28 +0900

    add_column_total

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         authentication_keys: [:name]
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: {maximum: 50}
   include DeviseTokenAuth::Concerns::User
 
   has_many :posts
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
